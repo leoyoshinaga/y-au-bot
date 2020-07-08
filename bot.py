d479b850dc70508690f64ea5a72a33aae1a23591
Leo Yoshinaga <yoshinagaleo@icloud.com>
2020-07-07 20:28:06 -0400

    begins login

--- bot.py
+++ bot.py
@@ -1,16 +1,22 @@
 from selenium import webdriver
+from secrets import store
 import time
 
-store = {
-  'url': 'https://zenmarket.jp/en/auction.aspx?itemCode=e447723398'
-  # maxBid:
-}
 
 
 if __name__ == '__main__':
+  #open chrome
   driver = webdriver.Chrome('./chromedriver')
+  #goes to url
   driver.get(store['url'])
-  # while True:
+  #close pop up
+  time.sleep(1)
+  driver.find_element_by_xpath('//*[@id="signUpOfferWindow"]/div/div/div[1]/button').click()
+  #sign in
+  driver.find_element_by_xpath('//*[@id="header1_not_logged_menu"]/a[1]').click()
+  driver.find_element_by_xpath('//*[@id="tbxLogin"]').send_keys(store['email'])
 
+  # driver.find_element_by_xpath('').send_keys()
+  # while True:
   #   time.sleep()
 
